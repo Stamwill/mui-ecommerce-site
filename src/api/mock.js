9ae8c727ee7a369b0cb2b2afa74b1d52e6c517f9
William Stam <williamstam@gmail.com>
2023-02-18 22:25:52 +0000

    [mock] Add data for carousel

---
+++ src/api/mock.js
@@ -0,0 +1,12 @@
+export const CarouselItemData = [
+  {
+    name: 'Comfy Clothing',
+    description: 'Clothing Piece One',
+    img: 'https://images.unsplash.com/photo-1434389677669-e08b4cac3105?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=810&q=80',
+  },
+  {
+    name: 'Set Clothing',
+    description: 'Buy full sets of clothing',
+    img: 'https://images.unsplash.com/photo-1516762689617-e1cffcef479d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80',
+  },
+]
