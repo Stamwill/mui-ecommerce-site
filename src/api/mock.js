662458b9922a541c56b5de86a43d9cdf24c8c730
William Stam <williamstam@gmail.com>
2023-02-20 10:29:41 +0000

    [mock] Add data for navigation

--- src/api/mock.js
+++ src/api/mock.js
@@ -10,3 +10,14 @@ export const CarouselItemData = [
     img: 'https://images.unsplash.com/photo-1516762689617-e1cffcef479d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80',
   },
 ]
+
+export const MainNav = [
+  {
+    label: 'Women',
+    href: '#',
+  },
+  {
+    label: 'Men',
+    href: '#',
+  },
+]
