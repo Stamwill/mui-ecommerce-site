f8ce81b9152c865abf9931208ac070897d76ded8
William Stam <williamstam@gmail.com>
2023-02-27 12:45:39 +0000

    [App] Add imports, set up containers for showcase

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,10 @@
 import * as React from 'react'
-import { Container } from '@mui/system'
+import { showcaseMen, showcaseWomen } from 'api/mock'
+import { Container, Box } from '@mui/system'
 import Hero from 'blocks/Hero'
 import MuiCarousel from 'blocks/MuiCarousel'
 import Showcase from 'blocks/Showcase'
+import { Typography } from '@mui/material'
 
 const App = () => {
   return (
@@ -11,14 +13,28 @@ const App = () => {
         <title>Mui Ecommerce Site</title>
       </header>
 
-      <div>
-        <Hero />
-        <Container maxWidth="md">
-          <MuiCarousel />
-        </Container>
-      </div>
+      <Hero />
+      <Container maxWidth="md">
+        <MuiCarousel />
+      </Container>
 
-      <Showcase />
+      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant="h4" component="h4" sx={{ marginBottom: '2rem' }}>
+            Mens
+          </Typography>
+          <Showcase content={showcaseMen} />
+        </Box>
+      </Container>
+
+      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant="h4" component="h4" sx={{ marginBottom: '2rem' }}>
+            Womens
+          </Typography>
+          <Showcase content={showcaseWomen} />
+        </Box>
+      </Container>
     </div>
   )
 }
