67b20577be60afee050a65591c0340d1f6151bb7
William Stam <williamstam@gmail.com>
2023-03-01 11:45:36 +0000

    [mock] Add data for an imageDivider

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,3 +1,6 @@
+export const homeImageDivider =
+  'https://images.unsplash.com/photo-1560243563-062bfc001d68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+
 export const CarouselItemData = [
   {
     name: 'Comfy Clothing',
