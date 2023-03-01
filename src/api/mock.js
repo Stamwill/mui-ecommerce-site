d12495b8cbb1284df5b3203b3afe73d5b9f42835
William Stam <williamstam@gmail.com>
2023-03-01 13:46:07 +0000

    [mock] Add title to showcase and change name to description

--- src/api/mock.js
+++ src/api/mock.js
@@ -16,38 +16,46 @@ export const CarouselItemData = [
 
 export const showcaseMen = [
   {
-    name: 'Original',
+    title: 'Clothing Name',
+    description: 'Original',
     img: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
   },
   {
-    name: 'Original',
+    title: 'Clothing Name',
+    description: 'Original',
     img: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
   },
   {
-    name: 'Original',
+    title: 'Clothing Name',
+    description: 'Original',
     img: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
   },
   {
-    name: 'Original',
+    title: 'Clothing Name',
+    description: 'Original',
     img: 'https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
   },
 ]
 
 export const showcaseWomen = [
   {
-    name: '705',
+    title: 'Clothing Name',
+    description: '705',
     img: 'https://images.unsplash.com/photo-1618354691551-44de113f0164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
   },
   {
-    name: '705',
+    title: 'Clothing Name',
+    description: '705',
     img: 'https://images.unsplash.com/photo-1618354691551-44de113f0164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
   },
   {
-    name: '705',
+    title: 'Clothing Name',
+    description: '705',
     img: 'https://images.unsplash.com/photo-1618354691551-44de113f0164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
   },
   {
-    name: '705',
+    title: 'Clothing Name',
+    description: '705',
     img: 'https://images.unsplash.com/photo-1618354691551-44de113f0164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80',
   },
 ]
