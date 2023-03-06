12adfcc34e64c19a60407969a4b799cf48747ebf
William Stam <williamstam@gmail.com>
2023-03-06 11:49:17 +0000

    [ShoppingCart] Set up base ShoppingCart container

---
+++ src/containers/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const ShoppingCart = (props) => {
+  const {} = props
+
+  return (
+    <section>
+      <div>
+        <p>ShoppingCart</p>
+      </div>
+    </section>
+  )
+}
+
+export default ShoppingCart
