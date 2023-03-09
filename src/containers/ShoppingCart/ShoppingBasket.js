d647f99a3c1643889a0af5506e75c0f1368816f8
William Stam <williamstam@gmail.com>
2023-03-09 12:37:29 +0000

    [ShoppingBasket] Remove box styling, add AnchorStyling

--- src/containers/ShoppingCart/ShoppingBasket.js
+++ src/containers/ShoppingCart/ShoppingBasket.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { navigation } from 'api/mock'
 import {
+  styled,
   Box,
   Drawer,
   IconButton,
@@ -12,26 +13,38 @@ import {
 import CloseIcon from '@mui/icons-material/Close'
 import { GlobalStateContext, GlobalHandlersContext } from 'contexts/Global'
 
+const AnchorStyling = styled('div')(({ theme }) => ({
+  width: '250px',
+  [theme.breakpoints.up('sm')]: {
+    width: '400px',
+  },
+  [theme.breakpoints.up('md')]: {
+    width: '600px',
+  },
+}))
+
 const ShoppingBasket = (props) => {
   const { basketOpen } = React.useContext(GlobalStateContext)
   const { handleBasket } = React.useContext(GlobalHandlersContext)
 
-  const list = (anchor) => (
-    <Box sx={{ width: anchor === 'top' || anchor === 'bottom' ? '100%' : 250 }}>
-      <List>
-        <IconButton onClick={handleBasket}>
-          <CloseIcon />
-        </IconButton>
+  const list = () => (
+    <AnchorStyling>
+      <Box>
+        <List>
+          <IconButton onClick={handleBasket}>
+            <CloseIcon />
+          </IconButton>
 
-        {navigation.map((item, idx) => (
-          <ListItem key={idx} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={item.label} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+          {navigation.map((item, idx) => (
+            <ListItem key={idx} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </AnchorStyling>
   )
 
   return (
